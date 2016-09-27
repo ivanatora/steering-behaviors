5342362c5b098db5821c9fbc3b26bfab710bfda4
Ivan Petrushev <ivanatora@gmail.com>
2016-09-27 16:56:27 +0300

    begin with Cohesion

---
+++ cohesion/Mover.js
@@ -0,0 +1,79 @@
+window.Mover = function(point){
+    this.position = point;
+    this.acceleration = new Point(0, 0);
+    this.velocity = new Point(0, 0);
+
+    this.max_velocity = 5;
+    this.angle = 0;
+    this.max_steer_angle = 20;
+
+    this.shape = new Path({
+        segments: [[-5, -5], [-5, 5], [10, 0]],
+        fillColor: 'red'
+    })
+
+    this.applyForce = function(force){
+        this.acceleration = this.acceleration + force;
+    }
+
+    this.update = function(){
+        var target = null;
+
+        var local_center = new Point(0, 0);
+        var cnt_local_movers = 0;
+
+        for (var i = 0; i < active_movers.length; i++){
+            if (active_movers[i] == this) continue;
+            var distance = active_movers[i].position.getDistance(this.position);
+            if (distance < close_distance){
+                local_center.x += active_movers[i].position.x;
+                local_center.y += active_movers[i].position.y;
+                cnt_local_movers++;
+            }
+        }
+
+
+        if (cnt_local_movers > 0){ // seek local center
+            local_center.x = Math.floor(local_center.x / cnt_local_movers);
+            local_center.y = Math.floor(local_center.y / cnt_local_movers);
+            console.log('local_center', local_center)
+
+            target = local_center;
+        }
+        else { // random movement
+            target = new Point(0, 10);
+            // console.log(target)
+            var rand_angle = Math.random() * 360;
+            target = target.rotate(rand_angle);
+            target = this.position + target;
+            target = target - this.position;
+        }
+
+
+
+
+        var delta_angle = this.angle - target.angle;
+        this.shape.rotate(-delta_angle);
+        this.angle = target.angle;
+
+
+        this.applyForce(target);
+
+        this.velocity = this.velocity + this.acceleration;
+        if (this.velocity.length > this.max_velocity){
+            this.velocity.length = this.max_velocity;
+        }
+        this.acceleration.length = 0;
+        this.position = this.position + this.velocity;
+
+        this.constrainScreen();
+        this.shape.position = this.position;
+    }
+
+    this.constrainScreen = function(){
+        if (this.position.x > width) this.position.x = 0;
+        if (this.position.x < 0) this.position.x = width;
+        if (this.position.y > height) this.position.y = 0;
+        if (this.position.y < 0) this.position.y = height;
+    }
+}
---
+++ cohesion/custom.js
@@ -0,0 +1,49 @@
+width = view.size.width;
+height = view.size.height;
+frame_every = 2;
+
+active_movers = [];
+
+close_distance = 100;
+
+run = true;
+
+function setup(){
+
+}
+
+function onFrame(event) {
+    if (run == false) return; // play-pause
+    if (event.count % frame_every != 0) return; // 1 frame per X seconds
+
+    for (var i = 0; i < active_movers.length; i++){
+        active_movers[i].update();
+    }
+}
+
+function onMouseDown(event){
+    var mover = new Mover(event.point);
+    active_movers.push(mover)
+}
+
+function onMouseDrag(event){
+    var point = event.point;
+    if (point.x > width || point.x < 0 || point.y < 0 || point.y > height){
+        return;
+    }
+    var mover = new Mover(point);
+    active_movers.push(mover)
+}
+
+$(document).ready(function(){
+    setup();
+
+    $('#new').click(function(e){
+        e.preventDefault();
+
+        var x = Math.floor(Math.random() * width);
+        var y = Math.floor(Math.random() * height);
+        var mover = new Mover(new Point(x, y));
+        active_movers.push(mover)
+    })
+})
